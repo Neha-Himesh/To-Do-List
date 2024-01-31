67f65a192608012ec05de31fcb7335405b25dd56
Neha Himesh <nehahimesh.10@gmail.com>
2024-01-31 13:27:13 +0530

    Final Code Cleanup

--- schema.js
+++ schema.js
@@ -5,22 +5,22 @@ var findOrCreate              = require("mongoose-findorcreate");
 MONGOOSE.connect("mongodb://127.0.0.1:27017/ToDoList");
 
 const TODOSCHEMA = new MONGOOSE.Schema({
-  username       : {
-    type         : String,
-    required     : true,
-  },
-  email          : String,
-  password       : String,
-  googleId       : String,
-  facebookId     : String,
-  work_task_id   : String,
-  work_task      : String,
-  day_task_id    : String,
-  day_task       : String,
-  day_task_time  : String,
-  day_task_date  : String,
-  work_task_time : String,
-  work_task_date : String
+    username       : {
+        type         : String,
+        required     : true,
+    },
+    email          : String,
+    password       : String,
+    googleId       : String,
+    facebookId     : String,
+    work_task_id   : String,
+    work_task      : String,
+    day_task_id    : String,
+    day_task       : String,
+    day_task_time  : String,
+    day_task_date  : String,
+    work_task_time : String,
+    work_task_date : String
 });
 
 TODOSCHEMA.plugin(PASSPORT_LOCAL_MONGOOSE);
